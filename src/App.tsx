283ec4268a43a83422003686ea690c9c7b4a15b7
Kacper Zacharz <xxygrek@wp.pl>
2022-08-05 11:56:59 +0200

    update

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { DateTime } from "luxon";
+import { Button } from "react-bootstrap";
+function App() {
+  alert("TEST");
+  return (
+    <div className="App">
+      <Button>TEST BUTTON</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Consts.ts
@@ -0,0 +1,3 @@
+const AUTH_URL =
+  "http://localhost:1300https://protected-dawn-82633.herokuapp.com/";
+export { AUTH_URL };
