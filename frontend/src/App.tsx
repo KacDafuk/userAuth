0bdd3c22e62266a287160bd3ef1c650ab93f929d
Kacper Zacharz <xxygrek@wp.pl>
2022-08-05 13:14:34 +0200

    fetch test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import Routes from "./routes/Routes";
 import Navbar from "./components/navbar/Navbar";
+import { useEffect } from "react";
+import { AUTH_URL } from "./Consts";
 function App() {
+  useEffect(() => {
+    fetch(AUTH_URL);
+  });
   return (
     <>
-      <Navbar />
-      <Routes />
+      {/* <Navbar />
+      <Routes /> */}
     </>
   );
 }
