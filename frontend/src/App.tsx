5a8b59da6192503dcf4362317aa6c4c25b7ee0db
Kacper Zacharz <xxygrek@wp.pl>
2022-08-05 11:53:00 +0200

    button test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 import { DateTime } from "luxon";
+import { Button } from "react-bootstrap";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button>TEST BUTTON</Button>
+    </div>
+  );
 }
 
 export default App;
