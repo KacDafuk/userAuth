0bb11621a56766e08838752181deb957dea16fe7
Kacper Zacharz <xxygrek@wp.pl>
2022-08-05 13:34:19 +0200

    fetch post test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
 import Routes from "./routes/Routes";
 import Navbar from "./components/navbar/Navbar";
+import { useEffect } from "react";
+import { AUTH_URL } from "./Consts";
 function App() {
+  useEffect(() => {
+    console.log("start post ");
+    fetch(AUTH_URL + "/test", {
+      method: "POST",
+    })
+      .then((x) => {
+        console.log("FIRST THEN");
+        return x.json();
+      })
+      .then((data) => {
+        console.log("final");
+        console.log(data);
+      });
+  });
   return (
     <>
-      <Navbar />
-      <Routes />
+      {/* <Navbar />
+      <Routes /> */}
     </>
   );
 }
